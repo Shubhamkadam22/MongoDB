5f47703c9d034da8fc2c9d5a537af27c7cfb878a
Shubham Kadam <shubhamkadam22100@gmail.com>
2026-04-05 21:08:15 +0530

    commits

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+    .then(() => {
+        console.log("comnnection successfull")
+    })
+    .catch((err) => console.log(err));
+
+    async function main() {
+        await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    }
+
+const userSchema = new mongoose.Schema({
+  
+    name: String,
+    email: String,
+    age: Number,
+
+});
+
+const User = mongoose.model("user" , userSchema);
+
+const user2 = new User({
+    name: "sat",
+    email: "satth@example.com",
+    age: 5,
+});
+
+user2
+ .save()
+ .then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
